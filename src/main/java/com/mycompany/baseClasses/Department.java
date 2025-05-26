ac3597b357c4a1ac4d5974ff516ff73923bcb96e
Eleni Siskou <helensiskou04@gmail.com>
2025-05-26 16:37:18 +0300

    small UI change
    στην λίστα πρώτα κλικάρεισ ένα τμήμα και μετά πατάς το κουμπί Προβολή προφιλ για να αποθηκευεται σωστα το ονομα του τμηματος (για να αντιστοιχηθει με το σωστό,δικό του προφιλ). NOT FINISHED!

--- src/main/java/com/mycompany/baseClasses/Department.java
+++ src/main/java/com/mycompany/baseClasses/Department.java
@@ -1,6 +1,8 @@
 package com.mycompany.baseClasses;
 
 import java.util.List;
+import java.util.ArrayList;
+
 
 public class Department {
     public String name;
@@ -9,13 +11,33 @@ public class Department {
 
     public Department(String name) {
         this.name = name;
+        this.description = "Περιγραφή για το τμήμα " + name;
+        this.announcements = new ArrayList<>();
     }
 
     public void updateDescription(String description) {
+                this.description = description;
+
 
     }
 
     public void addAnnouncement(Announcement announcement) {
+            announcements.add(announcement);
+
+    }
+    
+    
+    public String getName() {
+        return name;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public List<Announcement> getAnnouncements() {
+        return announcements;
+    }
+    
 }
+
